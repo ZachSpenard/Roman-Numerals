c406aaa4d8c4a464b0f0531ec1424c22a032da36
Kayl Eubanks and Zach Spenard <kayl@>
2018-03-29 11:19:27 -0700

    It will properly calculate numbers 1 through 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
- $(document).ready(function(){
-   $("#Number-converter").submit(function(event){
+$(document).ready(function(){
+  $("#Number-converter").submit(function(event){
     //  debugger;
-     event.preventDefault();
-     var userInput = parseInt($("input#Roman-Numerals").val());
-     var answerString = "";
-     if (userInput === 1) {
-       answerString = "I";
-     } else if (userInput === 5) {
-       answerString = "V";
-     } else if (userInput === 10) {
-       answerString = "X";
-     } else if (userInput === 50) {
-       answerString = "L";
-     } else if (userInput === 100) {
-       answerString = "C";
-     } else if (userInput === 500) {
-       answerString = "D";
-     } else if (userInput === 1000) {
-       answerString = "M";
-     }
-     for (var i = 0; i < userInput; i++) {
-       answerString = answerString + "I";
-     }
+    event.preventDefault();
+    var userInput = parseInt($("input#Roman-Numerals").val());
+    var answerString = "";
+    if (userInput === 1) {
+      answerString = "I";
+    } else if (userInput === 5) {
+      answerString = "V";
+    } else if (userInput === 10) {
+      answerString = "X";
+    } else if (userInput === 50) {
+      answerString = "L";
+    } else if (userInput === 100) {
+      answerString = "C";
+    } else if (userInput === 500) {
+      answerString = "D";
+    } else if (userInput === 1000) {
+      answerString = "M";
+    } else {
+      debugger;
+      for (var i = 0; i < userInput; i++) {
+        if (answerString[i-1] === answerString[i-2] && answerString[i-2] === answerString[i-3] && stringLength >= 3)   {
+          answerString = "IV";
+        } else {
+          answerString = answerString + "I";
+          var stringLength = answerString.length;
+        }
+      }
+    }
 
-
-     $("#Output").text(answerString);
-   });
- });
+    $("#Output").text(answerString);
+  });
+});
